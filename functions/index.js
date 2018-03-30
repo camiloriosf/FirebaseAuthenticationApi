c06e43388f14931679559d54cbea59b9ba1eaf0f
camiloriosf <camilo.rios.f@gmail.com>
2018-03-30 20:34:51 -0300

    add express app

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.get('*', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const api = functions.https.onRequest((req, res) => {
+  if (!req.path) req.url = `/${req.url}`;
+  return app(req, res);
+});
+
+module.exports = {
+  api
+};
